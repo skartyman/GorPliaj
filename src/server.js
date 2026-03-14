dec8a92135511c24f4138b9783f232c5581047b0
skartyman <skartyman@gmail.com>
2026-03-14 23:38:21 +0200

    Change server listen address to 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const app = require('./app');
 const { PORT } = require('./config/env');
 
 function startServer() {
-  return app.listen(PORT, () => {
-    console.log(`ГорПляж app is running on http://localhost:${PORT}`);
+  return app.listen(PORT, "0.0.0.0", () => {
+    console.log(`ГорПляж app is running on http://0.0.0.0:${PORT}`);
   });
 }
 
