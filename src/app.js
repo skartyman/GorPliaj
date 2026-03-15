05bcea349b17fa1dd55f36a2fb13e6baa4c3d7fd
skartyman <skartyman@gmail.com>
2026-03-15 08:05:21 +0200

    Change publicRoutes to use '/api' prefix

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(publicRoutes);
+app.use('/api', publicRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/payments', paymentsRoutes);
 
