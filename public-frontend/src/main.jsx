7ec2372c2315cd48a16199ae577de6f87c6ddcb0
skartyman <skartyman@gmail.com>
2026-04-18 22:54:25 +0300

    feat: link manifest and register service worker for PWA support

--- public-frontend/src/main.jsx
+++ public-frontend/src/main.jsx
@@ -58,6 +58,14 @@ async function cleanupLegacyServiceWorkers() {
 
 cleanupLegacyServiceWorkers();
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered'))
+      .catch(err => console.log('SW error', err));
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
